e3cbd52e30799cee6e6eb4338098cd16b04bf219
belenamoribieta <99616187+belenamoribieta@users.noreply.github.com>
2022-04-03 21:39:12 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import { purple } from '@ant-design/colors';
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+console.log(purple);
+console.log(purple.primary);
+
